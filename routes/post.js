b545666b2c71d4a101d1ab78d685b3880b93b836
jthebat <taein007@gmail.com>
2022-06-13 15:23:47 +0900

    FEAT: 전체 게시글 조회에 몇 가지 sorting 추가, 게시글 상세조회에 방영일로부터 지난 년도 값 추가 API 들 URL 수정

--- routes/post.js
+++ routes/post.js
@@ -8,44 +8,60 @@ const router = express.Router();
 /**
  * 전체 게시글 불러오기 API.
  */
-//taein
-router.get("/posts", async (req, res) => { 
-   // [{ post의 내용. _id: ..., title: ..., content: ... }, { }, { }]
-    //const { postDate } = req.body;
 
-    const posts = await Post.find().sort({ createdAt: 'desc' }).exec(); //post라는 스키마에서 find, sort()날짜기준 내림차순 
-    //console.log(posts); []배열 안에 게시글 하나씩[{게시글1},{게시글2},{게시글3}]   
+router.get("/posts/:sort", async (req, res) => { 
+    try{
     
-    const user_ids = posts.map((writer) =>writer.user_id);
-    //console.log(user_ids);  //user_id만 추출. ['user_id1', 'user_id2', 'user_id3', ..]
-    // $in mongodb query
-    const userInfoById = await User.find({
-        user_id: { $in: user_ids },
-    }).exec()    
-         .then((user) =>user.reduce((prev, a) => ({...prev,[a.user_id]:{user_id:a.user_id,nickname:a.nickname}, }), {} )); 
-         //console.log(userInfoById);
-         //프론트에서 원하는데로 가공해주자
-    res.send({
-       posts: posts.map((a) => ({   
-           
-            post_id: a.post_id,
-            thumbnail_url: a.thumbnail_url,
-            onair_year:a.onair_year,
-            title: a.title,
-            ost_url: a.ost_url,
-            content: a.content,
-            createdAt: a.createdAt,
-            likes: a.likes,
-            like_users: a.like_users,
-            userInfo: userInfoById[a.user_id],
-        })),
-    });
-});
+        const {sort} = req.params;
+        let posts=[];
+        
+        if(sort==="d_time"){
+            posts = await Post.find().sort({ createdAt: 'desc' }).exec(); // sort()작성시간 기준 내림차순   
+        }else if(sort==="a_time"){
+            posts = await Post.find().sort({ createdAt: 'asc' }).exec(); // sort()작성기준 오름차순
+        }else if(sort==="likes"){
+            posts = await Post.find().sort({ likes: 'desc' }).exec(); // sort()좋아요 기준 내림차순
+        }else if(sort==="release_year"){
+            posts = await Post.find().sort({ onair_year: 'asc' }).exec(); // sort()방영일 기준 오름차순
+        }
+    
+        //console.log(posts); []배열 안에 게시글 하나씩[{게시글1},{게시글2},{게시글3}]   
+    
+        const user_ids = posts.map((writer) =>writer.user_id);
+        //console.log(user_ids);  //user_id만 추출. ['user_id1', 'user_id2', 'user_id3', ..]
+        // $in mongodb query
+        const userInfoById = await User.find({
+            user_id: { $in: user_ids },
+        }).exec()    
+            .then((user) =>user.reduce((prev, a) => ({...prev,[a.user_id]:{user_id:a.user_id,nickname:a.nickname}, }), {} )); 
+            //console.log(userInfoById);
+            //프론트에서 원하는데로 가공해주자
+        res.send({
+            posts: posts.map((a) => ({   
 
+                    post_id: a.post_id,
+                    thumbnail_url: a.thumbnail_url,
+                    onair_year:a.onair_year,
+                    title: a.title,
+                    ost_url: a.ost_url,
+                    content: a.content,
+                    createdAt: a.createdAt,
+                    likes: a.likes,
+                    like_users: a.like_users,
+                    userInfo: userInfoById[a.user_id],
+                })),
+        });     
+
+        }catch(error) {       
+            res.status(400).send({
+            errorMessage: "게시글 조회에 실패하였습니다.",
+        })
+    };
+
+});
 /**
  * 글 생성, 입력 API 
 */
-//taein
 const post_schema = Joi.object({
       
     title: Joi.string(),
@@ -61,9 +77,9 @@ const post_schema = Joi.object({
     },
     like_users: [String], //이 글에 좋아요를 누른 사람들의 id값으로 좋아요를 눌렀으면 배열에 들어간다.
 });
-
-router.post("/posts", authMiddleware, async (req, res) => {  
+router.post("/post", authMiddleware, async (req, res) => {  
     try {      
+        
         //const { user } = res.locals;
         const { title, user_id, thumbnail_url, onair_year, content, ost_url } = await post_schema.validateAsync(req.body); // body 정보가져옴                
         if(!(ost_url.includes("www.youtube.com")||ost_url.includes("youtu.be"))){
@@ -76,8 +92,7 @@ router.post("/posts", authMiddleware, async (req, res) => {
                 errorMessage: "년도 형식으로 입력해주세요."
             });
             return; 
-        }   
-        else if (content.search(/^[\s\S]{1,2000}$/) == -1) {
+        } else if (content.search(/^[\s\S]{1,2000}$/) == -1) {
             res.status(412).send({
                 errorMessage: "게시글 내용의 형식이 일치하지 않습니다."
             });
@@ -98,7 +113,7 @@ router.post("/posts", authMiddleware, async (req, res) => {
                 ost_url,
         });          
         res.status(201).json({ result: 'success', msg: '내 추천만화가 등록되었습니다.' });    
-
+        
         } catch (err) {
             console.log(err)
             res.status(400).send({
@@ -110,14 +125,34 @@ router.post("/posts", authMiddleware, async (req, res) => {
 /**
  * 게시글 수정 API 
 */
-//taein
-router.patch('/posts/:post_id/modify',authMiddleware,async (req, res) => {
+
+router.patch('/post/:post_id/',authMiddleware,async (req, res) => {
     try{
-        const { title, thumbnail_url, onair_year, content, ost_url,user_id } = req.body;
+        const { title, thumbnail_url, onair_year, content, ost_url, user_id } = await post_schema.validateAsync(req.body);        
         const {post_id} = req.params;
-        const post = await Post.findById(post_id);
-           
-        const modifyArticle = await Post.findByIdAndUpdate(post_id, {
+        
+        if(!(ost_url.includes("www.youtube.com")||ost_url.includes("youtu.be"))){
+            res.status(412).send({
+                errorMessage: 'youtube의 영상만 가능합니다.',
+            });   
+            return;     
+        } else if (!(onair_year>1900&&onair_year<2022)) {
+            res.status(412).send({
+                errorMessage: "년도 형식으로 입력해주세요."
+            });
+            return; 
+        } else if (content.search(/^[\s\S]{1,2000}$/) == -1) {
+            res.status(412).send({
+                errorMessage: "게시글 내용의 형식이 일치하지 않습니다."
+            });
+            return;
+        } else if (title.search(/^[\s]*$/) != -1 || content.search(/^[\s]*$/) != -1) {
+            res.status(412).send({
+                errorMessage: "공백으로만 이루어진 게시글은 작성할 수 없습니다."
+            });
+            return;
+        }          
+        await Post.findByIdAndUpdate(post_id, {
             $set: { 
                 title:title,
                 thumbnail_url:thumbnail_url,
@@ -141,30 +176,30 @@ router.patch('/posts/:post_id/modify',authMiddleware,async (req, res) => {
 /**
  * 게시글 삭제 기능 API 
 */
-//taein 
-router.delete('/posts/:post_id',authMiddleware, async (req, res) => {    
-    try {
-        const {post_id} = req.params;
-        const existsPost = await Post.findById(post_id);
-        //console.log(existsPost);
-       
-        if (existsPost) {                      
-                await Post.findByIdAndDelete(post_id); // postId 일치하는 것으로 삭제
-                res.status(200).json({
-                    result: 'success',
-                    msg: '글이 삭제되었습니다.',
+
+router.delete('/post/:post_id',authMiddleware, async (req, res) => {    
+    try{
+            const {post_id} = req.params;
+            const existsPost = await Post.findById(post_id);
+            //console.log(existsPost);
+        
+            if(existsPost) {                      
+                    await Post.findByIdAndDelete(post_id); // postId 일치하는 것으로 삭제
+                    res.status(200).json({
+                        result: 'success',
+                        msg: '글이 삭제되었습니다.',
+                    });
+                    return;
+            }
+            else{
+                // 올 일은 없지만, 멀티 세션으로 같은 글을 동시에 지우려고 했을때?---???
+                res.status(400).json({
+                    result: 'error',
+                    msg: '게시글이 이미 삭제되었습니다.',
                 });
                 return;
-         }
-         else {
-            // 올 일은 없지만, 멀티 세션으로 같은 글을 동시에 지우려고 했을때?---???
-            res.status(400).json({
-                result: 'error',
-                msg: '게시글이 이미 삭제되었습니다.',
-            });
-            return;
-        }       
-    } catch (error) {        
+            }       
+    }catch (error) {        
         res.status(400).send({
             errorMessage: '게시글 삭제에 실패하였습니다.',
         });
@@ -173,8 +208,8 @@ router.delete('/posts/:post_id',authMiddleware, async (req, res) => {
 /**
  * 좋아요 기능 API 
 */
-//taein
-router.patch('/posts/:post_id/like',authMiddleware,async (req, res) => {
+
+router.patch('/post/like/:post_id/',authMiddleware,async (req, res) => {
     try{
         const { user_id } = req.body;
         const {post_id} = req.params;
@@ -206,8 +241,7 @@ router.patch('/posts/:post_id/like',authMiddleware,async (req, res) => {
 /**
  * 게시글 상세조회 기능 API 
 */
-//taein
-router.get('/posts/:post_id', async (req, res) => {
+router.get('/post/detail/:post_id', async (req, res) => {
     try{
         const { post_id } = req.params; 
         
@@ -215,16 +249,19 @@ router.get('/posts/:post_id', async (req, res) => {
         //console.log(post);
         //console.log(post.user_id);
         const post_user = await User.findOne({user_id:{ $in: post.user_id } }).exec(); 
+        const current_year = new Date().getFullYear();
+        
         //console.log("user",post_user);
         const postInfo = {
-            post_id,
+            //post_id,
             title: post.title,
             thumbnail_url: post.thumbnail_url,
-            onair_year:post.onair_year,
+            onair_year: post.onair_year,
             content: post.content,
-            ost_url:post.ost_url,
+            ost_url: post.ost_url,
             user_id: post_user.user_id,
             nickname: post_user.nickname,
+            period: current_year-post.onair_year,
             created_at: post.createdAt,
             likes:post_user.likes
         };  
