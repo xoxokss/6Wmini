78aefa3a0d065f13ef6cd05fec6310790d238c17
xoxokss <sangseon14@gmail.com>
2022-06-14 11:56:39 +0900

    Squashed commit of the following:
    commit 9befde971b7809427114153bfd0ae9495b8ade3c
    Author: xoxokss <sangseon14@gmail.com>
    Date:   Tue Jun 14 11:55:08 2022 +0900
    
        REFACTOR : 불필요 코드 제거 및 코드 정렬
    
    commit 4cab71cfd032b3c8690af1af8df640ac27addfc3
    Merge: 5986c93 c9f3d4a
    Author: xoxokss <sangseon14@gmail.com>
    Date:   Tue Jun 14 11:36:12 2022 +0900
    
        Merge branch 'main' into sangseon
    
    commit 5986c9374d1efe84c9ad851154f38567f6dc2af2
    Author: xoxokss <sangseon14@gmail.com>
    Date:   Tue Jun 14 11:23:48 2022 +0900
    
        REFACTOR : 댓글 조회, 작성 API 수정
    
    commit d1c8d8a36dcae8f72eb020a9a1b1e1e6f950b4de
    Author: xoxokss <sangseon14@gmail.com>
    Date:   Mon Jun 13 20:55:18 2022 +0900
    
        REFACTOR : 불필요 코드 삭제

--- models/comment.js
+++ models/comment.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema(
-    {
-        post_id: String,
-        user_id: String,
-        nickname: String,
-        comment: String,
-        // created_at : { type : Date, default : Date.now},
-    },
-   { timestamps: { createdAt: 'created_at'}}, // 자동 타임스탬프가 작동하는지 확인하기
+  {
+    post_id: String,
+    user_id: String,
+    nickname: String,
+    comment: String,
+    // created_at : { type : Date, default : Date.now},
+  },
+  { timestamps: { createdAt: "created_at" } } // 자동 타임스탬프가 작동하는지 확인하기
 );
 
-CommentSchema.virtual('comment_id').get(function () {
-    return this._id.toHexString();
+CommentSchema.virtual("comment_id").get(function () {
+  return this.c_id.toHexString();
 });
-CommentSchema.set('toJSON', {
-    virtuals: true,
+CommentSchema.set("toJSON", {
+  virtuals: true,
 });
 
-module.exports = mongoose.model('Comment', CommentSchema);
+module.exports = mongoose.model("Comment", CommentSchema);
